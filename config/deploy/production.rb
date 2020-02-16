39ae1487332807b075fc4194f26d26ed6c538462
Bouafia19 <khaledsetif1900@gmail.com>
2020-02-16 19:43:05 +0100

    test deploy to local server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 #104.248.40.94
-server '192.168.1.38', user: 'deploy', roles: %w{app db web}
+server '192.168.1.39', user: 'deploy', roles: %w{app db web}
 # role-based syntax
 # ==================
 
